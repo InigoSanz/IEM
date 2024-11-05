68e6031b99b8cffe260df638baf8e0c13bdbef2e
InigoSanz <i.sanzdelg@gmail.com>
2024-11-05 20:56:49 +0100

    Update EjercicioShakira.java
    Corrección final del ejercicio de Shakira.

--- src/primeros_ejercicios/EjercicioShakira.java
+++ src/primeros_ejercicios/EjercicioShakira.java
@@ -9,9 +9,12 @@ public class EjercicioShakira {
 
 	public static void main(String[] args) {
 		
-		int secondsOfVideo = (3 * 60) + 57; // Se podría crear una variable minutos-secgundos, 
-											// fragmentar la operación en variables.
-		double kcalPerSecond = 20.0 / 12.0;
+		int secondsOfVideo = (3 * 60) + 57; // Se podría crear una variable minutos-segundos, 
+											// fragmentar la operación en variables
+		
+		double kcalPerSecond = 20.0 / 12.0; // Aquí como no comprobé el resultado del total de los segundos
+											// del video, no me dí cuenta del fallo a la hora de calcular las
+											// Kcal por segundo
 		
 		double totalKcal = kcalPerSecond * secondsOfVideo;
 		
