81e322794e49e62a96fb34d87afa18ecfdd999be
InigoSanz <i.sanzdelg@gmail.com>
2024-11-12 17:32:14 +0100

    Ejercicio 12/11/2024
    Ejercicio 9

---
+++ src/ejercicios_12_11/Ejercicio9.java
@@ -0,0 +1,22 @@
+package ejercicios_12_11;
+
+/**
+ * Queremos calcular cuanto tiempo tarda en llegar un rayo de sol a la superficie de la tierra.
+ * - El movimiento de la luz en el vacío es de: 300.000 km/s
+ * - Distancia entre la Tierra y el Sol: 150.000.000 km
+ */
+public class Ejercicio9 {
+
+	public static void main(String[] args) {
+		
+		double distance = 150000000;
+		double speed = 300000;
+		
+		// Fórmula tiempo t = d/v
+		double t = distance / speed;
+		
+		System.out.printf("El tiempo que tarda en llegar un rayo de sol a la tierra es de %.2f segundos", t);
+
+	}
+
+}
