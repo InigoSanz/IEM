9ec360844a5ba1d54ab0af2b02fda8ca9aa8d077
InigoSanz <i.sanzdelg@gmail.com>
2024-11-13 19:00:58 +0100

    Ejercicios estructuras de control 13/11/2024
    Ejercicio 11

---
+++ src/ejercicios_EC_13_11/Ejercicio11.java
@@ -0,0 +1,29 @@
+package ejercicios_EC_13_11;
+
+import java.util.Scanner;
+
+/**
+ * Pedir al usuario 2 números enteros y devolver el mayor de ellos con el texto:
+ * - El mayor de los dos números es: {mayor}
+ */
+public class Ejercicio11 {
+
+	public static void main(String[] args) {
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.printf("Introduce un número (se aceptan decimales): ");
+		
+		double n1 = scanner.nextDouble();
+		
+		System.out.printf("Introduce un otro número (se aceptan decimales): ");
+		
+		double n2 = scanner.nextDouble();
+		
+		String mensaje = (n1 > n2) ? "El mayor de los dos números es: " + n1 : "El mayor de los dos números es: " + n2;
+		
+		System.out.printf("%s", mensaje);
+
+	}
+
+}
